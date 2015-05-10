c8d41754ba03cafb64d2e10ed0a604f83fdc49f9
Mike <mikhail.g.novikov@gmail.com>
2015-05-10 22:49:27 +0300

    added testing test

---
+++ tests/test_example.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import unittest
+from datetime import datetime
+import pdb
+import re
+
+
+from unipath import Path
+PROJECT_DIR = Path(__file__).ancestor(2)
+sys.path.append(PROJECT_DIR)
+
+from app import app
+
+
+
+
+class SomeTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.client = app.test_client()
+
+
+    def test_main_returns_200(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
